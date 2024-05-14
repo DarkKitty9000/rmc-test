1b09c949f7e3b1e271e7e949d11f9132a5b1ad54
Mikhail Nikiforov <nikiforov.mikhail.v@gmail.com>
2024-05-14 10:28:06 +0700

    Добавлен метод получения пользователя

--- app/crud.py
+++ app/crud.py
@@ -9,11 +9,16 @@ def get_nomenclature_placing_from_db(db: Session):
 
 
 def get_nomenclature_placing_from_db_for_user(db: Session, token: str):
-    try:
-        user = db.query(models.Token).filter(models.Token.token == token).first()
-    except:
-        raise HTTPException(status_code=404, detail="Пользователь не найден.")
+    user = get_user_by_token(db=db, token=token)
+    
     try:
         return db.query(models.NomenclaturePlacing).filter(models.NomenclaturePlacing.owner_id == user.user_id)
     except:
         return None
+    
+
+def get_user_by_token(db: Session, token: str):
+    try:
+        return db.query(models.Token).filter(models.Token.token == token).first()
+    except:
+        raise HTTPException(status_code=404, detail="Пользователь не найден.")
